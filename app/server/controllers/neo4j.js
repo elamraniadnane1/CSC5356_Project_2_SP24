da9a2a12551b5d5222d64fea01bfacb3d26e5cc4
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2024-03-29 12:33:58 +0000

    update

--- app/server/controllers/neo4j.js
+++ app/server/controllers/neo4j.js
@@ -1,23 +1,23 @@
 import neo4j from 'neo4j-driver'
 
-const driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', 'lina2015'))
+const driver = neo4j.driver('bolt://localhost:7687/neo4j', neo4j.auth.basic('ayoub', '123456789'))
 
 const getPostsFromNeo4j = async (userHashTags) => {
     const session = driver.session()
     try {
         const result = await session.run(
-            `MATCH (t:Tweet)-[:CONTAINS]->(h:Hashtag)
-            WHERE h.name IN $tags
-            RETURN t.text AS tweet, t.createdAt AS postedOn
-            ORDER BY t.createdAt DESC
-            `,
-            { tags: userHashTags }
+            `    MATCH (t:Tweet)
+            WHERE t.text CONTAINS '#Neo4j'
+            RETURN t.text AS TweetText, t.id AS UserId
+            LIMIT 100
+            `
+            // { tags: userHashTags }
         )
-        const posts = result.records.map((record) => ({
-            tweet: record.get('tweet'),
-            postedOn: record.get('postedOn')
-        }))
-        return posts
+        // const posts = result.records.map((record) => ({
+        //     tweet: record.get('tweet'),
+        //     postedOn: record.get('postedOn')
+        // }))
+        return result
     } finally {
         await session.close()
     }
